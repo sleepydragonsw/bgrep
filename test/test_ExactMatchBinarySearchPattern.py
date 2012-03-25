ad1c3c9197c69704235adb3b99d012c76c04d15d
Denver Coneybeare <denver@sleepydragon.org>
2012-03-25 15:51:16 -0400

    more unit tests for ExactMatchBinarySearchPattern

--- test/test_ExactMatchBinarySearchPattern.py
+++ test/test_ExactMatchBinarySearchPattern.py
@@ -2,19 +2,7 @@ import unittest
 
 from bgrep import ExactMatchBinarySearchPattern
 
-class Test(unittest.TestCase):
-
-    def test_EmptyNeedle_EmptyHaystack(self):
-        self.do_test_match_expected(b"", b"", 0, 0, 0, 0, None)
-
-    def test_EmptyNeedle_NonEmptyHaystack(self):
-        self.do_test_match_expected(b"", b"abc", 0, 0, 0, 0, None)
-
-    def test_EmptyNeedle_NonEmptyHaystack_NonZeroOffset(self):
-        self.do_test_match_expected(b"", b"abc", 2, 0, 2, 0, None)
-
-    def test_NeedleA_HaystackA(self):
-        self.do_test_match_expected(b"A", b"A", 0, 0, 0, 0, None)
+class ExactMatchBinarySearchPatternTestCase(unittest.TestCase):
 
     def do_test_match_expected(self, needle, haystack, haystack_offset,
             haystack_length, expected_offset, expected_length, expected_state):
@@ -29,3 +17,65 @@ class Test(unittest.TestCase):
             self.assertIsNone(actual_state)
         else:
             self.assertEqual(expected_state, actual_state)
+
+    def do_test_no_match_expected(self, needle, haystack, haystack_offset,
+            haystack_length):
+        x = ExactMatchBinarySearchPattern(needle)
+        result = x.search(haystack, haystack_offset, haystack_length)
+        self.assertIsNone(result)
+
+
+class TestBasicFunctionality(ExactMatchBinarySearchPatternTestCase):
+
+    def test01(self):
+        self.do_test_match_expected(b"a", b"a", 0, None, 0, 1, None)
+
+    def test02(self):
+        self.do_test_match_expected(b"abc", b"abc", 0, None, 0, 3, None)
+
+    def test03(self):
+        self.do_test_match_expected(b"abc", b"abcabc", 0, None, 0, 3, None)
+
+    def test04(self):
+        self.do_test_match_expected(b"abc", b"abcabc", 1, None, 3, 3, None)
+
+    def test05(self):
+        self.do_test_match_expected(b"abc", b"abcabc", 3, None, 3, 3, None)
+
+    def test06(self):
+        self.do_test_match_expected(b"abc", b"abcabc", 0, 3, 0, 3, None)
+
+    def test07(self):
+        self.do_test_no_match_expected(b"abc", b"abcabc", 1, 3)
+
+    def test08(self):
+        self.do_test_no_match_expected(b"abc", b"", 0, 0)
+
+
+class TestEmptySearchPattern(ExactMatchBinarySearchPatternTestCase):
+
+    def test_empty_haystack(self):
+        self.do_test_match_expected(b"", b"", 0, None, 0, 0, None)
+
+    def test_haystack_length1(self):
+        self.do_test_match_expected(b"", b"a", 0, None, 0, 0, None)
+
+    def test_haystack_length2(self):
+        self.do_test_match_expected(b"", b"ab", 0, None, 0, 0, None)
+
+    def test_haystack_offset1_length0(self):
+        self.do_test_match_expected(b"", b"abc", 1, 0, 1, 0, None)
+
+    def test_haystack_offset1_length1(self):
+        self.do_test_match_expected(b"", b"abc", 1, 1, 1, 0, None)
+
+    def test_haystack_offset_equals_len(self):
+        self.do_test_match_expected(b"", b"abc", 3, 0, 3, 0, None)
+
+    def test_haystack_offset_greater_than_len_length0(self):
+        self.do_test_no_match_expected(b"", b"abc", 4, 0)
+
+    def test_haystack_offset_greater_than_len_length1(self):
+        self.do_test_no_match_expected(b"", b"abc", 4, 1)
+
+
